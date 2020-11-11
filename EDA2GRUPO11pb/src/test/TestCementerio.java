e72333bdcab2f86ce5142b003d3800ecae817eea
JonathanGarello <jgarello@alumno.unlam.edu.ar>
2020-11-10 23:36:21 -0300

    mejoras del test

--- EDA2GRUPO11pb/src/test/TestCementerio.java
+++ EDA2GRUPO11pb/src/test/TestCementerio.java
@@ -245,6 +245,120 @@ import org.junit.Test;
 		Integer valorObtenido = osario.contarCantidadDifuntosEnOsario();
 		assertEquals(valorEsperado, valorObtenido);
 	}
+	
+	@Test
+	public void testQueSePuedaPasarDeTierraAOsario() {  
+		//el cementerio se encarga de pasar los difuntos, de tierra a osario
+		Empleado nuevo = new Empleado("Marcos", "Paz", 27654123);
+		Cementerio chacarita = new Cementerio("Chacarita");
+		nuevo.setCodigoPersonal(1);
+		chacarita.cargarEmpleado(nuevo);
+		chacarita.abrirElCementerio();
+		
+		Cliente hugo = new Cliente("Hugo", "Jimenez", 37987562);
+		
+		Cliente pedro = new Cliente("Pedro", "Porcel", 37987512);
+		
+		Difunto familiar2 = new Difunto("Carlos", "Porcel", 10476345, "18/05/1924", "06/11/2020");
+		pedro.setDifunto(familiar2);
+		
+		Difunto familiar = new Difunto("Pablo", "Jimenez", 10876345, "12/08/1920", "18/02/2018");
+		hugo.setDifunto(familiar);
+		
+		Cliente marcos = new Cliente("Marcos", "Gomez", 37987589);
+		Cliente jose = new Cliente("Jose", "Gomez", 37957589);
+		Difunto familiar4 = new Difunto("Raul", "Gomez", 20834345, "01/01/1929", "01/01/2020");
+		Difunto familiar3 = new Difunto("Ximena", "Estevez", 10834345, "06/09/1926", "04/12/2019");
+		marcos.setDifunto(familiar3);
+		jose.setDifunto(familiar4);
+		
+		chacarita.depositarEnOsario(marcos);
+		chacarita.depositarEnNichoUnDifunto(jose);
+		chacarita.enterrarDifunto(hugo);
+		chacarita.enterrarDifunto(pedro);
+		
+		Boolean exito = chacarita.pasajeDeTierraAOsario(familiar);
+		
+		
+		
+		assertTrue(exito);
+		
+	}
+	@Test
+	public void testQueSePuedaPasarDeNichoAOsario() {  
+		//el cementerio se encarga de pasar los difuntos, de nicho a osario
+		Empleado nuevo = new Empleado("Marcos", "Paz", 27654123);
+		Cementerio chacarita = new Cementerio("Chacarita");
+		nuevo.setCodigoPersonal(1);
+		chacarita.cargarEmpleado(nuevo);
+		chacarita.abrirElCementerio();
+		
+		Cliente hugo = new Cliente("Hugo", "Jimenez", 37987562);
+		
+		Cliente pedro = new Cliente("Pedro", "Porcel", 37987512);
+		
+		Difunto familiar2 = new Difunto("Carlos", "Porcel", 10476345, "18/05/1924", "06/11/2020");
+		pedro.setDifunto(familiar2);
+		
+		Difunto familiar = new Difunto("Pablo", "Jimenez", 10876345, "12/08/1920", "18/02/2018");
+		hugo.setDifunto(familiar);
+		
+		Cliente marcos = new Cliente("Marcos", "Gomez", 37987589);
+		Cliente jose = new Cliente("Jose", "Gomez", 37957589);
+		Difunto familiar4 = new Difunto("Raul", "Gomez", 20834345, "01/01/1929", "01/01/2020");
+		Difunto familiar3 = new Difunto("Ximena", "Estevez", 10834345, "06/09/1926", "04/12/2019");
+		marcos.setDifunto(familiar3);
+		jose.setDifunto(familiar4);
+		
+		chacarita.depositarEnOsario(marcos);
+		chacarita.depositarEnNichoUnDifunto(jose);
+		chacarita.enterrarDifunto(hugo);
+		chacarita.enterrarDifunto(pedro);
+		
+		Boolean a = chacarita.pasajeDeNichoAOsario(familiar4);
+		
+		assertTrue(a);
+		
+		
+	}
+	
+	@Test
+	public void testQueSeVeaLaListaDeClientes() { 
+				Empleado nuevo = new Empleado("Marcos", "Paz", 27654123);
+				Cementerio chacarita = new Cementerio("Chacarita");
+				nuevo.setCodigoPersonal(1);
+				chacarita.cargarEmpleado(nuevo);
+				chacarita.abrirElCementerio();
+				
+				Cliente hugo = new Cliente("Hugo", "Jimenez", 37987562);
+				
+				Cliente pedro = new Cliente("Pedro", "Porcel", 37987512);
+				
+				Difunto familiar2 = new Difunto("Carlos", "Porcel", 10476345, "18/05/1924", "06/11/2020");
+				pedro.setDifunto(familiar2);
+				
+				Difunto familiar = new Difunto("Pablo", "Jimenez", 10876345, "12/08/1920", "18/02/2018");
+				hugo.setDifunto(familiar);
+				
+				Cliente marcos = new Cliente("Marcos", "Gomez", 37987589);
+				Cliente jose = new Cliente("Jose", "Gomez", 37957589);
+				Difunto familiar4 = new Difunto("Raul", "Gomez", 20834345, "01/01/1929", "01/01/2020");
+				Difunto familiar3 = new Difunto("Ximena", "Estevez", 10834345, "06/09/1926", "04/12/2019");
+				marcos.setDifunto(familiar3);
+				jose.setDifunto(familiar4);
+				
+				chacarita.cargarNuevoCliente(jose);
+				chacarita.cargarNuevoCliente(marcos);
+				chacarita.cargarNuevoCliente(pedro);
+				chacarita.cargarNuevoCliente(hugo);
+				
+				chacarita.depositarEnOsario(marcos);
+				chacarita.depositarEnNichoUnDifunto(jose);
+				chacarita.enterrarDifunto(hugo);
+				chacarita.enterrarDifunto(pedro);
+				
+				System.out.println(chacarita.verListaDeClientes());
+	}
 		
 		
 
